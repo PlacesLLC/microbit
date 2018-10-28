2772ddbf2e6faf22a21d9456eb8cc4e79f55cd36
PlacesLLC <35419439+PlacesLLC@users.noreply.github.com>
2018-10-28 23:06:29 +0800

    更新。

---
+++ loading.ts
@@ -0,0 +1,138 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # . # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        . . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        . # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # . # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # . # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # . # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # . #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . .
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . .
+        # # # # #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # .
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # . #
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # . # #
+        `)
+    basic.pause(10)
+})
